8cba9c0c1f39f5e614b92acbd5e519a5eb163d10
Aparimeya Taneja <at307@duke.edu>
2019-06-19 10:29:17 +0530

    lint sucks

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/ting', (req,res) => {
+    console.log('heres');
+    
+  res.send({
+        message:'hello world'
+  })
+})
+app.listen(process.env.PORT || 3000)
